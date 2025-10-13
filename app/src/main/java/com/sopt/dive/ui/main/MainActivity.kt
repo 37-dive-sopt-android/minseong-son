4b32e5d2079f620fc1b5c940d959454fef144cda
minseong-PC <smsdia412@gmail.com>
2025-10-13 17:58:51 +0900

    feat: Add user profile UI to MainActivity
    - Implement a user profile section in `MainScreen`.
    - Display a profile image, name, and a short bio.
    - Add `UserInfoHolder`, a new composable to display labeled user information like ID, Password, Nickname, and Alcohol tolerance.
    - Add a new profile image drawable resource.

--- app/src/main/java/com/sopt/dive/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/dive/ui/main/MainActivity.kt
@@ -4,17 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sopt.dive.R
 import com.sopt.dive.ui.theme.DiveTheme
 
 /**
@@ -65,13 +82,87 @@ fun MainScreen(
             .padding(paddingValues),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "프로필 사진",
+                modifier = Modifier
+                    .size(36.dp)
+                    .clip(CircleShape)
+                    .background(
+                        color = Color.White,
+                        shape = CircleShape
+                    )
+                    .border(
+                        width = 2.dp,
+                        color = Color.Black,
+                        shape = CircleShape
+                    )
+            )
 
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = "손민성",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "안녕하세요 손민성입니다!",
+            fontSize = 16.sp
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        UserInfoHolder(
+            label = "ID",
+            info = userId
+        )
+
+        UserInfoHolder(
+            label = "PASSWORD",
+            info = userPassword
+        )
+
+        UserInfoHolder(
+            label = "NICKNAME",
+            info = userNickname
+        )
+
+        UserInfoHolder(
+            label = "주량",
+            info = userAlcohol
+        )
     }
 }
 
 @Composable
 private fun UserInfoHolder(
+    label : String,
     info : String
 ) {
+    Text(
+        text = label,
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier
+            .fillMaxWidth(),
+        textAlign = TextAlign.Start
+    )
 
+    Text(
+        text = info,
+        fontSize = 16.sp,
+        modifier = Modifier
+            .fillMaxWidth(),
+        textAlign = TextAlign.Start
+    )
+
+    Spacer(modifier = Modifier.height(16.dp))
 }
