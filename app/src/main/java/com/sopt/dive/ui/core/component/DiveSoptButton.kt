f5e3d9c5b559b465924ce96fb0874e68ed6404db
minseong-PC <smsdia412@gmail.com>
2025-10-13 17:51:44 +0900

    feat: Add DiveSoptButton composable
    - Create `DiveSoptButton`, a custom button component.
    - The button fills the maximum width and has a blue background.
    - It takes a text string and an onClick lambda as parameters.

---
+++ app/src/main/java/com/sopt/dive/ui/core/component/DiveSoptButton.kt
@@ -0,0 +1,30 @@
+package com.sopt.dive.ui.core.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DiveSoptButton(
+    text : String,
+    onClickButton : () -> Unit
+) {
+    Button(
+        onClick = onClickButton,
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Blue
+        )
+    ) {
+        Text(
+            text = text,
+            fontSize = 14.sp,
+        )
+    }
+}
