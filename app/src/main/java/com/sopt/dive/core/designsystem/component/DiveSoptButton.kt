cd06f84bca8dfe46eafa6fd561953bafa0b8aa9d
minseong-PC <smsdia412@gmail.com>
2025-10-15 15:29:59 +0900

    refactor: Remove ripple effect from DiveSoptButton
    - Remove the ripple effect on click by setting the `interactionSource` to `NoRippleInteractionSource`.
    - Add a `modifier` parameter to allow for external modifications.

--- app/src/main/java/com/sopt/dive/core/designsystem/component/DiveSoptButton.kt
+++ app/src/main/java/com/sopt/dive/core/designsystem/component/DiveSoptButton.kt
@@ -5,22 +5,26 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
+import com.sopt.dive.core.util.NoRippleInteractionSource
 
 @Composable
 fun DiveSoptButton(
     text : String,
-    onClickButton : () -> Unit
+    onClickButton : () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Button(
         onClick = onClickButton,
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth(),
         colors = ButtonDefaults.buttonColors(
             containerColor = Color.Blue
-        )
+        ),
+        interactionSource = remember { NoRippleInteractionSource() }
     ) {
         Text(
             text = text,
