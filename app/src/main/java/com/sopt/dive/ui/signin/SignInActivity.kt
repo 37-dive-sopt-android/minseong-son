d16e35ddcbe83a30ac8cb0c0688996742b3460a2
sonms <smsms5676@naver.com>
2025-10-17 20:50:41 +0900

    refactor: Remove unused FocusManager import
    - Delete the import for `androidx.compose.ui.focus.FocusManager` in `SignInActivity` as it is no longer in use.

--- app/src/main/java/com/sopt/dive/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/sopt/dive/ui/signin/SignInActivity.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalFocusManager
