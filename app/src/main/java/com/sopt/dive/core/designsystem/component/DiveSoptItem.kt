92375a86b48f5fe53db15320e81477899264192b
minseong-PC <smsdia412@gmail.com>
2025-10-21 15:45:12 +0900

    feat: `DiveSoptItem` 컴포저블 추가 및 `DiveSoptTextField` 개선
    - Card를 뒤집는 플립 애니메이션을 포함한 새로운 `DiveSoptItem` 컴포저블을 추가합니다.
    - `DiveSoptTextField`의 `inputTransformation` 및 `outputTransformation` 파라미터에 `null` 기본값을 설정하여 선택적으로 사용할 수 있도록 수정합니다.

---
+++ app/src/main/java/com/sopt/dive/core/designsystem/component/DiveSoptItem.kt
@@ -0,0 +1,87 @@
+package com.sopt.dive.core.designsystem.component
+
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.dive.core.designsystem.theme.DiveTheme
+
+@Composable
+fun DiveSoptItem(
+    data: String,
+    modifier: Modifier = Modifier
+) {
+    var isFlipped by remember { mutableStateOf(false) }
+
+    val rotationY by animateFloatAsState(
+        targetValue = if (isFlipped) 180f else 0f,
+        label = "rotationY",
+        animationSpec = tween(
+            durationMillis = 1000,
+            easing = FastOutSlowInEasing
+        )
+    )
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { isFlipped = !isFlipped }
+            .graphicsLayer {
+                this.rotationY = rotationY
+                cameraDistance = 12f * density
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            if (rotationY <= 90f) {
+                Text(
+                    text = data,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            } else {
+                Text(
+                    text = "뒤집힌 면",
+                    modifier = Modifier.graphicsLayer {
+                        this.rotationY = 180f
+                    }.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun DiveSoptItemPreview() {
+    DiveTheme {
+        DiveSoptItem(
+            data = "sopt"
+        )
+    }
+}
