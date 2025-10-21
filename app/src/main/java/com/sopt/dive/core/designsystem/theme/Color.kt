8912b2eba5d6f3b212ed3b0fb939269fb2c1b6a8
minseong-PC <smsdia412@gmail.com>
2025-10-21 15:46:22 +0900

    refactor: Remove unused SuppressLint annotation and add new color
    - Remove `@SuppressLint("AutoboxingStateCreation")` from `ModifierExtensions.kt` as it is no longer necessary.
    - Add the `DeepPurple80` color to `Color.kt`.

--- app/src/main/java/com/sopt/dive/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/sopt/dive/core/designsystem/theme/Color.kt
@@ -3,7 +3,9 @@ package com.sopt.dive.core.designsystem.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val DeepPurple80 = Color(0xFF7A4BB4)
 val PurpleGrey80 = Color(0xFFCCC2DC)
+
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
