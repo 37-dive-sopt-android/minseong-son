2f463f311d3e35a5008a7b6c7f033fb1385fa9b6
minseong-PC <smsdia412@gmail.com>
2025-11-14 17:31:33 +0900

    feat: Coil ImageLoader 설정을 위한 Application 클래스 추가
    - `DiveSoptApplication` 클래스를 추가하여 `Application`을 상속하고 `ImageLoaderFactory`를 구현했습니다.
    - `newImageLoader` 메서드를 오버라이드하여 Coil의 `ImageLoader` 인스턴스를 생성하고, 앱 전반에 걸쳐 이미지 로딩 설정을 중앙에서 관리하도록 했습니다.
    - 이미지 로딩 시 크로스페이드 효과(`crossfade`)를 활성화했습니다.
    - 메모리 캐시는 전체 메모리의 25%를 사용하도록, 디스크 캐시는 최대 50MB 크기로 `image_cache` 디렉토리에 저장되도록 설정했습니다.
    - 디버깅을 위해 `DebugLogger`를 활성화하고, HTTP 캐시 헤더를 무시하도록 `respectCacheHeaders(false)` 옵션을 추가했습니다.

---
+++ app/src/main/java/com/sopt/dive/DiveSoptApplication.kt
@@ -0,0 +1,30 @@
+package com.sopt.dive
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.util.DebugLogger
+
+class DiveSoptApplication : Application(), ImageLoaderFactory {
+    // Todo : 나중에 di로 관리
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(50 * 1024 * 1024)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .respectCacheHeaders(false)
+            .build()
+    }
+}
