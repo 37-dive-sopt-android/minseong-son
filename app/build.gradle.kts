c86aea7e2705659a788fc27dd4ddae69115fd86d
minseong-PC <smsdia412@gmail.com>
2025-11-14 17:31:03 +0900

    feat: Room 및 Paging 라이브러리 종속성 추가
    - `gradle/libs.versions.toml`에 Room, Paging, KSP 관련 라이브러리 버전 및 종속성을 추가했습니다.
    - Room과 Paging 라이브러리를 각각 `room`, `paging` 번들로 그룹화했습니다.
    - 최상위 `build.gradle.kts`와 `app/build.gradle.kts`에 `ksp` 플러그인을 적용했습니다.
    - Room의 스키마 위치를 지정하는 `ksp` 설정(`room.schemaLocation`)을 추가했습니다.
    - `app/build.gradle.kts`에 Room과 Paging 라이브러리 종속성을 구현부에 추가하고, `ksp`를 통해 Room 컴파일러를 적용했습니다.
    - 중복되는 `com.intellij:annotations` 의존성을 제거하기 위해 `configurations.all` 블록을 추가했습니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 val properties = Properties().apply {
@@ -47,21 +48,34 @@ android {
             jvmTarget = JvmTarget.JVM_11
         }
     }
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
+    }
     buildFeatures {
         compose = true
         buildConfig = true
     }
 }
 
+
+
 dependencies {
     implementation(libs.bundles.androidx)
-    implementation(libs.bundles.test)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.kotlinx)
     implementation(platform(libs.okhttp.bom))
     implementation(libs.bundles.okhttp)
     implementation(libs.bundles.retrofit)
-
-    debugImplementation(libs.bundles.debug)
     implementation(libs.coil.compose)
+    implementation(libs.bundles.room)
+    implementation(libs.bundles.paging)
+    ksp(libs.room.compiler)
+    debugImplementation(libs.bundles.debug)
+    testImplementation(libs.bundles.unit.test)
+    androidTestImplementation(libs.bundles.instrumented.test)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+}
+
+configurations.all {
+    exclude(group = "com.intellij", module = "annotations")
 }
